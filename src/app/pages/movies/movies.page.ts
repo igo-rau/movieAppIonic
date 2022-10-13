a6cb138c9c9f8e2ddbfe81527ec683bf7b67d85f
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-10-13 18:32:31 +0300

    Basic list of 20  items loads and works

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movies',
@@ -7,11 +9,27 @@ import { MovieService } from 'src/app/services/movie.service';
   styleUrls: ['./movies.page.scss'],
 })
 export class MoviesPage implements OnInit {
+  movies = [];
+  currentPage = 1;
+  imageBaseUrl = environment.images;
 
-  constructor(private movieService: MovieService) { }
+
+  constructor(private movieService: MovieService, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
-    this.movieService.getTopRatedMovies().subscribe(res => {
+    this.loadMovies();
+  }
+  
+  async loadMovies() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      spinner: 'bubbles'
+    });
+    await loading.present();
+
+    this.movieService.getTopRatedMovies(this.currentPage).subscribe(res => {
+      loading.dismiss();
+      this.movies = [...this.movies, ...res.results];
       console.log(res);
     })
     this.movieService.getMovieDetails('550').subscribe(res => {
